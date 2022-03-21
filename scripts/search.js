9b93d6a9e62df59d5256f2c60b6ddb2f1a73cc9b
Matthew Nazari <matthewanazari@gmail.com>
2022-03-20 21:28:01 -0400

    add initial files

---
+++ scripts/search.js
@@ -0,0 +1,68 @@
+const VERBS = []
+
+let template = `
+<h1>
+  <span class="term"></span>
+  <span class="pattern"></span>
+  <span class="def"></span>
+  <span class="root"></span>
+</h1>
+<div class="pres"></div>
+<div class="past"></div>
+<div class="rp"></div>
+<div class="imp"></div>
+<div class="inf"></div>
+<div class="prog"></div>
+<div class="vn"></div>
+<div class="ap"></div>
+`
+
+class Verb {
+  constructor(verb) {
+    this.verb = verb
+    this.search_fields = [this.verb.def]
+    this.elem = document.createElement('section')
+    this.elem.innerHTML = `
+    <h1>
+      <span class="term">${this.verb['term']}</span>
+      <span class="pattern">${this.verb['pattern']}</span>
+      <span class="def">${this.verb['def']}</span>
+      <span class="root">${this.verb['root']}</span>
+    </h1>
+    <div class="pres">${this.verb['forms']['pres']}</div>
+    <div class="pst">${this.verb['forms']['pst']}</div>
+    <div class="rsp">${this.verb['forms']['rsp']}</div>
+    <div class="imper">${this.verb['forms']['imper']}</div>
+    <div class="inf">${this.verb['forms']['inf']}</div>
+    <div class="prog">${this.verb['forms']['prog']}</div>
+    <div class="vn">${this.verb['forms']['vn']}</div>
+    <div class="ap">${this.verb['forms']['ap']}</div>
+    `
+    document.querySelector('main').append(this.elem)
+    this.hide()
+    VERBS.push(this)
+  }
+  relates_to = search => search && this.search_fields.some(tag => tag.toLowerCase().includes(search.toLowerCase()))
+  hide = _ => this.elem.style.display = 'none'
+  show = _ => this.elem.style.display = 'block'
+}
+
+filter = async _ => {
+  search = document.querySelector('input').value
+  for (const verb of VERBS) {
+    await verb.relates_to(search) ? verb.show() : verb.hide()
+  }
+  //  VERBS.forEach(verb => verb.relates_to(search) ? verb.show() : verb.hide())
+}
+
+fetch('data.json')
+  .then(response => response.text())
+  .then(text => JSON.parse(text))
+  .then(json => json.forEach(verb => new Verb(verb)))
+
+
+// fetch('recipes/published_recipes.md')
+//   .then(response => response.text())
+//   .then(text => [...text.matchAll(/# ([^\n]+)(?:\s*- id: *([a-z]*))(?:\s*- tags: *([a-z\d, ]*))?/gi)])
+//   .then(matches => matches.map(match => match.slice(1)))
+//   .then(recipes => recipes.forEach(recipe => new Recipe(recipe)))
